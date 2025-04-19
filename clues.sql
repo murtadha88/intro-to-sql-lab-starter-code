cff4333e33c8ab582b56e156ddb6a1b5674a153b
Murtadha Almoamen <murtadhaalmoamen8@gmail.com>
2025-04-19 09:53:48 +0300

    lab completed

--- clues.sql
+++ clues.sql
@@ -4,11 +4,13 @@
 SELECT code, name, population FROM countries 
 WHERE region = 'Southern Europe' ORDER BY population LIMIT 1;
 
+
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
 SELECT language FROM countrylanguages 
 WHERE countrycode = 'VAT';
+
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
@@ -22,6 +24,7 @@ AND code IN (
     GROUP BY countrycode 
     HAVING COUNT(*) = 1
 );
+
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
